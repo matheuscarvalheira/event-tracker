290e8d264c4c571573c0e8d48699d78c1ce144f4
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2022-12-22 13:09:37 -0300

    using hook to delete event

--- src/components/Evento/index.tsx
+++ src/components/Evento/index.tsx
@@ -4,17 +4,14 @@ import { IEvento } from "../../interfaces/IEvento";
 import { listaDeEventosState } from "../../state/atom";
 import style from "./Evento.module.scss";
 import EventoCheckbox from "./EventoCheckbox";
+import useDeletarEvento from "../../state/hooks/useDeletarEvento";
 
 const Evento: React.FC<{
   evento: IEvento;
 }> = ({ evento }) => {
   const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
 
-  const excluirEvento = () => {
-    setListaDeEventos((listaAntiga) =>
-      listaAntiga.filter((e) => e.id !== evento.id)
-    );
-  };
+  const excluirEvento = useDeletarEvento();
 
   const estilos = [style.Evento];
 
@@ -30,7 +27,7 @@ const Evento: React.FC<{
           {evento.descricao} - {evento.inicio.toLocaleDateString()}
         </h3>
       </div>
-      <i className="far fa-times-circle fa-2x" onClick={excluirEvento}></i>
+      <i className="far fa-times-circle fa-2x" onClick={() => excluirEvento(evento)}></i>
     </div>
   );
 };
